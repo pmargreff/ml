eb0af9070d9b51367b7a1451e4765d1830511545
pmargreff <pmargreff@gmail.com>
2016-09-22 19:35:22 -0300

    [feature] - reading all files and group by dir name

--- naive_bayes/naive_bayes.jl
+++ naive_bayes/naive_bayes.jl
@@ -1,25 +1,40 @@
 # TODO:
 # 1 - implementar tokenizer - DONE
-# 2 - implementar o leitor e separador de arquivo
+# 2 - implementar o leitor e separador de arquivo - DONE
 # 3 - entender algoritmo de classificação
 # 4 - implementar trainamento
 # 5 - implementar classificador
 
 function tokenizer(text)
   words = String[]
-  sentence = (split(s))
+  sentence = (split(text))
   for word in sentence
-    if isalnum(word)
+    if isalpha(word)
       push!(words, lowercase(word))
     end
   end
   return words  
 end
 
-f = open("data/ham/0002.1999-12-13.farmer.ham.txt");
-s = readstring(f)
+function readData(dataPath)
+  dataDir = (readdir(dataPath))
+  dict = Dict()
+  for dir in dataDir
+    subFold = string(dataPath,"/",dir)
+    files = (readdir(subFold))
+    allWords = String[]
+    
+      for fileName in files 
+      filepath = string(subFold,"/",fileName)
+      f = open(filepath);
+      text = readstring(f)
+      append!(allWords, tokenizer(text))
+      close(f)
+    end
+    
+    dict[dir] = allWords
+  end
+  return dict
+end
 
-println(tokenizer(s))
-
-
-close(f)
+readData("./data")
