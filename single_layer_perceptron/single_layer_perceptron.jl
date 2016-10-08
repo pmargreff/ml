8e373d6d693985f10d9c2ce8d4745e0b04b2a020
pmargreff <pmargreff@gmail.com>
2016-10-07 23:55:28 -0300

    [feature] - test the training to all possible class and list the wrong matches

--- single_layer_perceptron/single_layer_perceptron.jl
+++ single_layer_perceptron/single_layer_perceptron.jl
@@ -7,12 +7,12 @@ end
 
 function sum_values(x,w)
   total = 0.0
-
+  
   for i in 1:length(x)
     total += x[i] * w[i]
   end
   return total 
-
+  
 end
 
 function get_response(x, w)
@@ -24,7 +24,7 @@ end
 function train(df,target,eta,eras)
   
   nrows, ncolumns = size(df)
-  
+  label = String
   learned = false
   perceptron = Perceptron([],[])
   perceptron.w = rand(Float64,ncolumns)
@@ -46,7 +46,7 @@ function train(df,target,eta,eras)
       push!(perceptron.x,1) 
       append!(perceptron.x, convert(Array,df[row,2:ncolumns])) 
       response = get_response(perceptron.x,perceptron.w)
-
+      
       if response != class
         iterError = class - response
         for i in 1:ncolumns
@@ -58,15 +58,90 @@ function train(df,target,eta,eras)
     
     era += 1
     
-    if globalError == 0.0 || era > eras
-      println("Perceptron weights: " ,perceptron.w)
-      println("Learned in ", era , " eras")
+    if globalError == 0.0 || era >= eras
+      println("Label " , target ," learned in ", era , " eras")
+      saveTrain(target, perceptron.w)
       learned = true
     end
-
+    
   end
 end
 
+function saveTrain(label, w)
+  iterator = 1
+  
+  outfile = string("train/",label,".csv")
+  file = open(outfile, "w")
+  
+  for value in w 
+    print(file, value)
+    if iterator != length(w)
+      print(file, ",")
+      iterator += 1
+    end
+  end
+  close(file)
+end
 
-df = readtable("data/fast_mnist_train.csv", header = false)
-train(df,1,0.1,1000)
+function getTrainings()
+  files = (readdir("train/"))
+  
+  train = Dict()
+  
+  for file in files 
+    filename = splitext(file)
+    filepath = string("train/",file)
+    train_df = readtable(filepath, header = false)
+    train_array = convert(Array, train_df)
+    train[filename[1]] = train_array
+  end
+  
+  return train
+end
+
+function getHit(class, example)
+  example[1] = 1
+  total = sum(class.*example)
+  return total
+end
+
+function getLabel(train, example)
+  example_array = convert(Array,example)
+  
+  major_acc = 0.0
+  major_class = ""
+  
+  for class in train 
+    hit = getHit(class[2], example_array)
+    if hit > major_acc
+      major_acc = hit
+      major_class = class[1]
+    end
+  end
+  
+  return example[1], parse(Int64,major_class)
+  
+end
+
+function test(file)
+  
+  trainings = getTrainings()
+  
+  df = readtable(file, header = false)
+  i = 1
+  for row in eachrow(df)
+    correct, attempt = getLabel(trainings, row)
+    if correct != attempt
+      println(i , " erro: ", correct ," ", attempt)
+    end
+    i += 1
+  end
+  
+end
+
+df = readtable("data/mnist_train.csv", header = false)
+for i in 0:9
+  train(df,i,0.001,10000)
+end
+
+# test("data/fast_mnist_train.csv")
