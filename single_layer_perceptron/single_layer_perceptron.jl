e546f5c5c99c5e30b737186a7b5cc3428ee27bce
pmargreff <pmargreff@gmail.com>
2016-10-09 12:20:08 -0300

    [feature] - delta rule implemented

--- single_layer_perceptron/single_layer_perceptron.jl
+++ single_layer_perceptron/single_layer_perceptron.jl
@@ -11,7 +11,7 @@ function get_response(x, w)
   return res
 end
 
-function train(df,target,eta,eras)
+function training_rule(df,target,eta,eras)
   
   nrows, ncolumns = size(df)
   label = String
@@ -50,6 +50,59 @@ function train(df,target,eta,eras)
     println("Era " , era ," finished, error: " , globalError)
     
     if globalError < 0.15 || era >= eras
+      println("Label " , target ," learned in ", era , " eras")
+      saveTrain(target, perceptron.w)
+      learned = true
+    end
+    
+    println("-------------------------------------------------")
+    era += 1
+    
+  end
+end
+
+function delta_rule(df,target,eta,eras)
+  nrows, ncolumns = size(df)
+  label = String
+  learned = false
+  perceptron = Perceptron([],[])
+  perceptron.w = rand(Float64,ncolumns)
+  era = 1
+  while !learned
+    println("Training era ",era)
+    globalError = 0.0
+    deltaw[] = 0 * rand(Float64,ncolumns)
+    for row in 1:nrows
+      label = (convert(Array,df[row,1:1]))
+      label = label[1]
+      
+      class = Float64
+      label == target ? class = 1.0 : class = -1.0 
+      
+      if row != 1 || era != 1
+        splice!(perceptron.x,1:ncolumns)
+      end
+      
+      push!(perceptron.x,1) 
+      append!(perceptron.x, convert(Array,df[row,2:ncolumns])) 
+      response = get_response(perceptron.x,perceptron.w)
+      
+      if response != class
+        iterError = class - response
+        for i in 1:ncolumns
+          deltaw[i] += eta * iterError * perceptron.x[i]
+        end
+        globalError += abs(iterError)   
+      end
+    end
+    
+    for i in 1:ncolumns
+      perceptron.w[i] += deltaw[i]
+    end
+    
+    println("Era " , era ," finished, error: " , globalError)
+    
+    if globalError < 50 || era >= eras
       println("Label " , target ," learned in ", era , " eras")
       saveTrain(target, perceptron.w)
       learned = true
@@ -145,7 +198,7 @@ if length(ARGS) == 2
   if ARGS[1] == "train"
     df = readtable(ARGS[2], header = false)
     for i in 0:9
-      train(df,i,0.1,100)
+      training_rule(df,i,0.1,100)
     end
   elseif ARGS[1] == "test"
     test(ARGS[2])
